70d55ae5da2ada29f002e7e3f1d545907297922d
mengli <limengxiang1986@163.com>
2022-05-23 21:38:52 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { PureComponent } from "react";
+
+class App extends PureComponent{
+  render(){
+    return (
+      <div>
+        header
+        body
+        footer
+      </div>
+    )
+  }
+}
+
+export default App
